93732bf8d3390dd05676cc5ac2137f628077da62
g4ld <ana.parada@nordstrom.com>
2021-06-12 21:57:46 -0700

    added newman and maxsub

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -3,6 +3,14 @@
 # Space Complexity: ?
 def max_sub_array(nums)
     return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+
+    current_max = nums[0]
+    max_ending_here = 0
+
+    nums.each do |n|
+      max_ending_here = [n, max_ending_here + n].max
+      current_max = [current_max, max_ending_here].max
+    end
+    current_max
+
 end
--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,18 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: o(n)
+# Space Complexity: o(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num <= 0
+  return "1" if num == 1
+
+  result = [0, 1, 1]
+  i = 3
+
+  while i <= num
+    result << result[result[i - 1]] + result[i - result[i - 1]]
+    i += 1
+  end
+
+  result[1..-1].join(' ')
 end
